27cca3a2187e4a8ef11248b719b936d55d618d1d
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-12-21 00:58:50 +0200

    Adding cube animation

---
+++ src/components/rubikComponent.js
@@ -0,0 +1,8 @@
+const rubikComponent = () => {
+  const rubikElement = document.createElement('div');
+  rubikElement.classList.add('rubik');
+
+  return rubikElement;
+};
+
+export default rubikComponent;
