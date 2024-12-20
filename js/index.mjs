42f2805e1ec4106bc9fe589f61e5cdb2671dc312
Yaroslav Chaplinskyi <yaroslavch7@gmail.com>
2024-12-20 17:37:22 +0200

    Adding cubes

--- js/index.mjs
+++ js/index.mjs
@@ -1,12 +1,3 @@
 import anime from '../node_modules/animejs/lib/anime.es.js';
 
-const animeElRef = document.querySelector('.js-anime-el');
-
-anime({
-  targets: animeElRef,
-  translateX: 250,
-  rotate: '1turn',
-  backgroundColor: '#FFF',
-  duration: 8000,
-  loop: true,
-});
+const cubes = document.querySelectorAll('.cube');
